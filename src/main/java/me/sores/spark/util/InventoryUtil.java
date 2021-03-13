f533325348215f52a015e91276230ce58b8e186a
sores <drewmax1234@yahoo.com>
2021-03-12 21:59:40 -0500

    Serialization
    Add Inventory and PotionEffect serialization methods.

--- src/main/java/me/sores/spark/util/InventoryUtil.java
+++ src/main/java/me/sores/spark/util/InventoryUtil.java
@@ -1,9 +1,21 @@
 package me.sores.spark.util;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sores on 3/11/2020.
@@ -41,6 +53,15 @@ public class InventoryUtil {
             return true;
         }
         return false;
+    }
+
+    public static ItemStack[] fixInventoryOrder(ItemStack[] source) {
+        ItemStack[] fixed = new ItemStack[36];
+
+        System.arraycopy(source, 0, fixed, 27, 9);
+        System.arraycopy(source, 9, fixed, 0, 27);
+
+        return fixed;
     }
 
 }
