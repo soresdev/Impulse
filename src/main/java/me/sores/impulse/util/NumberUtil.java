1dc70904a79342a219c8f4496a98bc85cca21a38
sores <drewmax1234@yahoo.com>
2021-04-23 01:56:47 -0400

    Update NumberUtil.java

--- src/main/java/me/sores/impulse/util/NumberUtil.java
+++ src/main/java/me/sores/impulse/util/NumberUtil.java
@@ -1,5 +1,7 @@
 package me.sores.impulse.util;
 
+import me.sores.impulse.Impulse;
+
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
@@ -7,6 +9,19 @@ import java.text.NumberFormat;
  * Created by sores on 3/3/2020.
  */
 public class NumberUtil {
+
+    /**
+     * Check if characters are numerical
+     * @param str - String to check
+     * @return
+     */
+    public static boolean isNumerical(String str) {
+        return str.matches("[0-9]+");
+    }
+
+    public static int getInRangeDiff(int min, int max) {
+        return Impulse.RAND.nextInt(max + 1 -min) + min;
+    }
 
     /**
      * Formats numbers by like 1000000 to 1,000,000
@@ -28,6 +43,20 @@ public class NumberUtil {
         try {
             int i = Integer.parseInt(str);
         } catch (NumberFormatException ex){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check if a number can be a Double
+     * @param str - String to check
+     * @return
+     */
+    public static boolean isDouble(String str) {
+        try {
+            double i = Double.parseDouble(str);
+        } catch (NumberFormatException ex) {
             return false;
         }
         return true;
